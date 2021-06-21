7d4e30479e30a4f2fc2163cf88f587ba4b1c076b
mahnoor-awan <mahnoorawan014@gmail.com>
2021-06-22 00:24:34 +0500

    initial commit

--- app/src/main/java/com/example/assignment2/Login.java
+++ app/src/main/java/com/example/assignment2/Login.java
@@ -1,10 +1,27 @@
 package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.assignment2.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Login extends AppCompatActivity {
+    FirebaseAuth user;
+    String email,password;
+    EditText user_email, user_password;
+    TextView signup;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/assignment2/LoginIntro.java
+++ app/src/main/java/com/example/assignment2/LoginIntro.java
@@ -1,14 +1,55 @@
 package com.example.assignment2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
+import com.example.assignment2.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
 public class LoginIntro extends AppCompatActivity {
+    Button get_start;
+    FirebaseAuth fireBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_intro);
+        fireBase = FirebaseAuth.getInstance();
+        if(fireBase.getCurrentUser() != null)
+        {
+            Toast.makeText(LoginIntro.this,"Already logged in!",Toast.LENGTH_SHORT).show();
+            redirect("MAIN");
+        }
+        get_start = findViewById(R.id.btnGetStarted);
+        get_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirect("LOGIN");
+            }
+        });
+    }
+    private void redirect(String name){
+        if(name.equals("LOGIN"))
+        {
+            Intent intent = new Intent(LoginIntro.this, Login.class);
+                startActivity(intent);
+                finish();
+        }
+        else if (name.equals("MAIN")){
+            Intent intent=new Intent(LoginIntro.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            throw new java.lang.RuntimeException("Not Bad");
+        }
     }
 }
